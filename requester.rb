589547e18879aef794a647a7972b7637a0e09695
Nicholas Labarre <bitbucket@ndlr.xyz>
2017-08-05 23:03:58 -0400

    removed now useless URI checks in requester

--- requester.rb
+++ requester.rb
@@ -2,18 +2,9 @@ require 'net/http'
 require 'uri'
 
 class Requester
-  def self.make_request(request_config, url)
-    route = request_config["route"]
+  def self.make_request(request_config)
+    uri = request_config["uri"]
 
-    unless route
-      abort("route must be specified in request config")
-    end
-
-    unless route.start_with?('/')
-      route.prepend('/')
-    end
-
-    uri = URI.parse("http://" + url + route)
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' => 'application/json'})
     request.body = request_config["body"].to_json
